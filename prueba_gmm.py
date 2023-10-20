5b8680178a1a6a1b08caeffbcf21c98393827b37
Leonardo Isidro <leoisidro@MacBook-Air-de-Leonardo.local>
2023-10-20 12:05:49 -0500

    GMM terminado

--- prueba_gmm.py
+++ prueba_gmm.py
@@ -3,39 +3,92 @@ from GMM import GMM,np
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
-from  sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_samples
+from sklearn.metrics import silhouette_score
 
-# Genera un dataset de ejemplo
-n_samples = 189
-n_features = 5
+import pandas as pd
+
+names = pd.read_csv("./clase.txt")
+
+dataset = pd.read_csv("./dataset_tissue.txt", skiprows=1, header=None).drop(0, axis=1).transpose()
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
+
+scaler = RobustScaler()
+X_scaled = scaler.fit_transform(dataset)
+
+pca = PCA(n_components=66)
+df_pca = pca.fit_transform(X_scaled)                   
+
+
+X = df_pca
+Y = names.x.values
 n_components = 7
-X, y = make_blobs(n_samples=n_samples, n_features=n_features, centers=n_components, random_state=42)
+
+
+dic_clases={}
+cont=0
+for i in range(len(Y)):
+    if Y[i] not in dic_clases:
+        dic_clases[Y[i]] = (cont,[i])
+        cont+=1
+    else:
+        dic_clases[Y[i]][1].append(i)
+
 
 # Crea el modelo GMM
 gmm = GMM(n_clusters=n_components)
 
 # Ajusta el modelo a los datos
 gmm.entrenamiento(X)
-print("Y :",y)
 
 # Predice las etiquetas de los clusters para cada punto de datos
 labels = gmm.prediccion(X)
-print(len(labels))
-print("labels :",labels)
+
+print(labels)
+
+silhouette_vals = silhouette_samples(X, labels)
+
+# Crea una figura para visualizar la imagen de silueta
+fig, ax = plt.subplots()
+y_lower = 10
+
+for i in range(n_components):
+    cluster_silhouette_vals = silhouette_vals[labels == i]
+    cluster_silhouette_vals.sort()
+    y_upper = y_lower + cluster_silhouette_vals.shape[0]
+
+    color = plt.cm.viridis(float(i) / n_components)
+    ax.fill_betweenx(np.arange(y_lower, y_upper), 0, cluster_silhouette_vals, facecolor=color, edgecolor=color, alpha=0.7)
+    ax.text(-0.05, y_lower + 0.5 * cluster_silhouette_vals.shape[0], str(i))
+    y_lower = y_upper + 10
 
 
-# Obtiene los parámetros aprendidos del modelo
+ax.set_xlabel("Coeficiente de Silueta")
+ax.set_ylabel("Etiqueta del Clúster")
+ax.set_title("Gráfico de Silueta para Clustering GMM",)
+
+# Línea vertical para el valor promedio de silueta en todos los datos
+average_silhouette_score = silhouette_score(X, labels)
+ax.axvline(x=average_silhouette_score, color="red", linestyle="--")
+ax.set_yticks([])  # Borrar etiquetas en el eje y
+
+print("score:", average_silhouette_score)
+plt.show()
+
+"""
 means = gmm.means
-covariances = gmm.covarianza
-
-
+covariances = gmm.covariances
 
 # Plotea los resultados
 plt.figure(figsize=(10, 5))
 
 # Datos de entrada
 plt.subplot(1, 2, 1)
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap='viridis')
+plt.scatter(X[:, 0], X[:, 1], c=Y, cmap='viridis')
 plt.title("Datos de entrada")
 
 # Resultados del GMM
@@ -55,5 +108,5 @@ for i, (mean, cov) in enumerate(zip(means, covariances)):
     ell.set_alpha(0.5)
     plt.gca().add_artist(ell)
 
-plt.show()
+plt.show()"""
 
